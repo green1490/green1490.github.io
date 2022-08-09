a4b34e51d3241c31712e5cd716dc3beb48d47fc7
green1490 <zambokrisztian1490@gmail.com>
2022-08-09 16:24:27 +0200

    import navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Sidebar from './components/Sidebar.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <Sidebar></Sidebar>
+  <Sidebar/>
+  <Navbar/>
 </template>
 
 
