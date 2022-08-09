b6315a372085db838909c5dd036d0f31c0bea30e
green1490 <zambokrisztian1490@gmail.com>
2022-08-09 16:49:30 +0200

    import home tag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import Sidebar from './components/Sidebar.vue'
-import Navbar from './components/Navbar.vue'
+  import Sidebar from './components/Sidebar.vue'
+  import Navbar from './components/Navbar.vue'
+  import Home from './components/Home.vue'
 </script>
 
 <template>
   <Sidebar/>
   <Navbar/>
+  <Home/>
 </template>
 
 
