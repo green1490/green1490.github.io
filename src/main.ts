5a3193fc37df9773b58e5dabe08e612656dff19c
green1490 <zambokrisztian1490@gmail.com>
2022-08-16 16:41:03 +0200

    add boostrap-vue dephendency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-
 import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
 
 createApp(App)
     .use(router)
