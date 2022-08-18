5f64fa68ff3fedcc2c59fbf84265283a2543d832
green1490 <zambokrisztian1490@gmail.com>
2022-08-18 12:40:52 +0200

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import './assets/main.css'
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap/dist/js/bootstrap.js"
+import './index.css'
 
 createApp(App)
     .use(router)
