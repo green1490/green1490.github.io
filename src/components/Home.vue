ed13a23fc85ca11be3a17f5a3b090ccc8fef2d6a
green1490 <zambokrisztian1490@gmail.com>
2022-08-18 16:23:35 +0200

    scalable font

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Hi,</p>
-        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">I'm Zámbó Krisztián,</p>
-        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">software developer</p>
+        <p class="text-[10px] md:text-base lg:text-2xl xl:text-4xl 2xl:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Hi,</p>
+        <p class="text-[10px] md:text-base lg:text-2xl xl:text-4xl 2xl:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">I'm Zámbó Krisztián,</p>
+        <p class="text-[10px] md:text-base lg:text-2xl xl:text-4xl 2xl:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">software developer</p>
     </div>
 </template>
