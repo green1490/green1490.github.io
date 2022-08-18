47be2ceeaf67c36a4b5068223ee5aab43c86ca96
green1490 <zambokrisztian1490@gmail.com>
2022-08-18 12:46:08 +0200

    change font size

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,7 @@
 <template>
-    <div class="home">
-        <p>Hi,</p>
-        <p>I'm Zámbó Krisztián,</p>
-        <p>software developer</p>
+    <div>
+        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Hi,</p>
+        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">I'm Zámbó Krisztián,</p>
+        <p class="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">software developer</p>
     </div>
 </template>
-
-
-<style scoped>
-    .home p {
-        font-size: 2vw;
-    }
-</style>
