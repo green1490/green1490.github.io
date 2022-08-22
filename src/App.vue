9f5fbf40fdf9d7b97b6a760ecc983367f8908e33
green1490 <zambokrisztian1490@gmail.com>
2022-08-22 18:36:33 +0200

    remove navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-  import Sidebar from './components/Sidebar.vue'
   import Navbar from './components/Navbar.vue'
   import Home from './components/Home.vue'
 </script>
 
 <template>
-  <Sidebar/>
   <Navbar/>
   <Home/>
   <!-- <RouterLink to="/">Home</RouterLink>
