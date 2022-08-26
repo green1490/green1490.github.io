e344e22c094c4bfbbb9c0a2ea2dc5907269824b5
green1490 <zambokrisztian1490@gmail.com>
2022-08-26 20:16:40 +0200

    change about me text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,18 +1,11 @@
 <template>
-    <div class="about">
+    <div class="text-[10px] md:text-base xl:text-base 2xl:text-xl">
         <p>
-            test
+            Hello! My name is Krisztian and I enjoy creating things.
+            I am always learning all sort of stack for my projects.
+            I hope i always can learn more as a software developer.  
         </p>
+        <p>Check out my stacks:</p>
     </div>
 </template>
 
-<style scoped>
-.about {
-    font-size: 16px;
-}
-</style>
-
-<script lang="ts">
-export default {
-}
-</script>
